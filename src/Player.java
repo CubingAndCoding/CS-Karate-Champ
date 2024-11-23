41084d766518561b5da8c4b4e05d8f20d8153dc0
alaqmartrunkwala <alaqmartrunk@gmail.com>
2024-11-23 17:57:14 -0600

    Code Skeleton

--- src/Player.java
+++ src/Player.java
@@ -3,7 +3,7 @@ import java.awt.*;
 public class Player extends Sprite {
     @Override
     public void drawImage(Graphics2D g2) {
-
+        // draw the image on the Graphics2D reference
     }
 
     @Override
