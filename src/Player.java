7257ed057bb4a7ee11768add3f93704dd9928c0c
Alaqmar Trunkwala <alaqmartrunk@gmail.com>
2024-11-23 18:00:36 -0600

    Test Commit

--- src/Player.java
+++ src/Player.java
@@ -3,8 +3,7 @@ import java.awt.*;
 public class Player extends Sprite {
     @Override
     public void drawImage(Graphics2D g2) {
-        // draw the image on the Graphics2D reference
-        // redraw
+
     }
 
     @Override
